10959cb7ba19ebe32d2ecd261abc5eda4570a6e4
Rohith Chandra <rohithchandra12345@gmail.com>
2026-09-14 23:09:21 +0530

    Explore dataset with Pandas

--- scripts/explore_data.py
+++ scripts/explore_data.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 filepath = "data/raw/amazon_ecommerce_1M.csv"
 
-df = pd.read_csv(filepath, nrows=5)
+df = pd.read_csv(filepath, nrows=10000)
 
+print(df.shape)
+print(df.info())
 print(df.describe())
+print(df["category"].value_counts())
+print(df["category"].unique())
+print(df.isna().sum())
+print(df.duplicated().sum())
+print(df["device"].unique())
+print(df.dtypes)
