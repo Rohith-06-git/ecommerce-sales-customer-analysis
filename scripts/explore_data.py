1711f325606782b7ffbf57c4d3a51433059ebe1b
Rohith Chandra <rohithchandra12345@gmail.com>
2026-09-14 23:31:07 +0530

    Explore dataset with Pandas

--- scripts/explore_data.py
+++ scripts/explore_data.py
@@ -13,3 +13,7 @@ print(df.isna().sum())
 print(df.duplicated().sum())
 print(df["device"].unique())
 print(df.dtypes)
+
+print(df["purchase_date"].head())
+df["purchase_date"] = pd.to_datetime(df["purchase_date"])
+print(df.dtypes)
